5210509f518fe15b2101ed902d18c502cc597f80
Peter Dave Hello <hsu@peterdavehello.org>
2020-08-24 13:22:44 +0800

    Make cloudflare-set-real-ip.conf variable

--- generate.sh
+++ generate.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cf_ips="$(mktemp)"
+cp_ip_config="/etc/nginx/conf.d/cloudflare-set-real-ip.conf"
 
 curl --compressed -sLo- https://www.cloudflare.com/ips-v4 >> "$cf_ips"
 curl --compressed -sLo- https://www.cloudflare.com/ips-v6 >> "$cf_ips"
 sed -i -e 's/^/set_real_ip_from /g' -e 's/$/;/g' "$cf_ips"
 sed -i -e '1i real_ip_header CF-Connecting-IP;'  "$cf_ips"
 
-mv "$cf_ips" /etc/nginx/conf.d/cloudflare-set-real-ip.conf
+mv "$cf_ips" "$cp_ip_config"
