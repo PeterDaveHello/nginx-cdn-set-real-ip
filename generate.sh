4df0de41ee268836c78431531ef949628e8df4fb
Peter Dave Hello <hsu@peterdavehello.org>
2020-08-24 13:25:03 +0800

    Set config permission to 644 instead of mktemp default 600

--- generate.sh
+++ generate.sh
@@ -3,6 +3,8 @@
 cf_ips="$(mktemp)"
 cp_ip_config="/etc/nginx/conf.d/cloudflare-set-real-ip.conf"
 
+chmod 644 "$cf_ips"
+
 curl --compressed -sLo- https://www.cloudflare.com/ips-v4 >> "$cf_ips"
 curl --compressed -sLo- https://www.cloudflare.com/ips-v6 >> "$cf_ips"
 sed -i -e 's/^/set_real_ip_from /g' -e 's/$/;/g' "$cf_ips"
