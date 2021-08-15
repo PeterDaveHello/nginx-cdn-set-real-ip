45e33fa28ff676a2df5c391885ad93f9dd0422b7
Peter Dave Hello <hsu@peterdavehello.org>
2021-08-15 17:58:12 +0800

    Fix typo

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cf_ips="$(mktemp)"
-cp_ip_config="/etc/nginx/conf.d/cloudflare-set-real-ip.conf"
+cf_ip_config="/etc/nginx/conf.d/cloudflare-set-real-ip.conf"
 
 if [ "$1" = "--cron" ]; then
     sleep $((RANDOM % 900))
@@ -14,4 +14,4 @@ curl --compressed -sLo- https://www.cloudflare.com/ips-v6 >> "$cf_ips"
 sed -i -e 's/^/set_real_ip_from /g' -e 's/$/;/g' "$cf_ips"
 sed -i -e '1i real_ip_header CF-Connecting-IP;'  "$cf_ips"
 
-mv "$cf_ips" "$cp_ip_config"
+mv "$cf_ips" "$cf_ip_config"
