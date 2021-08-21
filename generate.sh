4934e54b2995aee4cf6db0d94e0afe4596ea7191
Peter Dave Hello <hsu@peterdavehello.org>
2021-08-21 19:08:33 +0800

    Insert addtion newline char to fix IP list

--- generate.sh
+++ generate.sh
@@ -10,7 +10,9 @@ fi
 chmod 644 "$cf_ips"
 
 curl --compressed -sLo- https://www.cloudflare.com/ips-v4 >> "$cf_ips"
+echo '' >> "$cf_ips"
 curl --compressed -sLo- https://www.cloudflare.com/ips-v6 >> "$cf_ips"
+echo '' >> "$cf_ips"
 sed -i -e 's/^/set_real_ip_from /g' -e 's/$/;/g' "$cf_ips"
 sed -i -e '1i real_ip_header CF-Connecting-IP;'  "$cf_ips"
 
