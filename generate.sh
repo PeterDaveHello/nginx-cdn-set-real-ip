4cedcab2f9860d65e1610d69f4722f0ce8232079
Peter Dave Hello <hsu@peterdavehello.org>
2023-03-27 01:45:06 +0800

    Improve user-friendliness of script output messages

--- generate.sh
+++ generate.sh
@@ -16,20 +16,24 @@ fi
 
 chmod 644 "$cf_ips"
 
+echo "Fetching Cloudflare IP addresses..."
+
 for file in ips-v4 ips-v6; do
     curl --compressed -sLo- "https://www.cloudflare.com/$file" >> "$cf_ips"
     echo '' >> "$cf_ips"
 done
 
+echo "Generating nginx configuration file..."
 sed -i -e 's/^/set_real_ip_from /g' -e 's/$/;/g' -e '1i real_ip_header CF-Connecting-IP;' "$cf_ips"
 
 if ! [ -e "$cf_ip_config" ]; then
     mv -f "$cf_ips" "$cf_ip_config"
-    echo "nginx config added."
+    echo "Nginx configuration for Cloudflare IP addresses added successfully."
 elif cmp -s "$cf_ips" "$cf_ip_config"; then
-    echo "No changes detected."
+    echo "No changes detected. We have nothing to do."
     rm -f "$cf_ips"
 else
+    echo "Cloudflare IP addresses config have changed. Updating nginx configuration..."
     mv -f "$cf_ips" "$cf_ip_config"
-    echo "nginx config updated."
+    echo "Nginx configuration for Cloudflare IP addresses updated successfully."
 fi
